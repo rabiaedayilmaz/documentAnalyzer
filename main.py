db9836ae01502f354630adb41c19523f7ebddeb5
Mustafa Durmus <mustafa-durmuss@outlook.com>
2023-08-08 12:10:31 +0300

    style: linting main.py

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.callbacks.base import BaseCallbackHandler
 import pickle
 
+
 class StreamHandler(BaseCallbackHandler):
     def __init__(self, container, initial_text=""):
         self.container = container
-        self.text=initial_text
+        self.text = initial_text
+
     def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.text+=token
+        self.text += token
         self.container.markdown(self.text)
 
 
@@ -20,28 +22,34 @@ st.title('Albert Kullanım Kılavuzuna Sor')
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 openai_org_id = st.secrets["OPENAI_ORGANIZATION_KEY"]
 
+
 @st.cache_data
 def load_data(path="guidelines_data/docs.index"):
-  index = faiss.read_index(path)
-  with open("guidelines_data/faiss_store.pkl", "rb") as f:
-      store = pickle.load(f)
-      return store, index
+    index = faiss.read_index(path)
+    with open("guidelines_data/faiss_store.pkl", "rb") as f:
+        store = pickle.load(f)
+        return store, index
+
 
 store, index = load_data()
 store.index = index
+
+
 def generate_response(input_text):
-  question_prompt = f"""
+    question_prompt = f"""
   You are Albert.
   Never forget to answer only in Turkish. 
   Question: {input_text}
   """
-  chat_box=st.empty()
-  stream_handler = StreamHandler(chat_box)
-  chain = RetrievalQAWithSourcesChain.from_chain_type(llm=ChatOpenAI(temperature=0.1, streaming=True, callbacks=[stream_handler]), retriever=store.as_retriever())
-  result = chain(question_prompt)
+    chat_box = st.empty()
+    stream_handler = StreamHandler(chat_box)
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=ChatOpenAI(temperature=0.1, streaming=True, callbacks=[stream_handler]), retriever=store.as_retriever())
+    chain(question_prompt)
+
 
 with st.form('my_form'):
-  text = st.text_area('Sorularını dinliyorum...', 'Albert, seni nasıl kullanabilirim?')
-  submitted = st.form_submit_button('Gönder')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+    text = st.text_area('Sorularını dinliyorum...', 'Albert, seni nasıl kullanabilirim?')
+    submitted = st.form_submit_button('Gönder')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
